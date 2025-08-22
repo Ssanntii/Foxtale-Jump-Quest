e0b1cdc880c8c5b73bbd849291c3def410f9213d
Santiago Ruiz Diaz | UAA-Sistemas <srdiaz@uaa.com.ar>
2025-08-22 11:00:02 -0300

    Tilemaps & added Inputs

--- Enemies/frog.gd
+++ Enemies/frog.gd
@@ -1,34 +1,40 @@
 extends CharacterBody2D
 
-const JUMP_VELOCITY = -300
+const JUMP_VELOCITY = -200
 var SPEED = 100
 var player
 var chase := false
+
 @onready var anim = $AnimatedSprite2D
 
 func _ready():
-	anim.play("Idle")
-	
+	anim.play("Idle")	
+
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+
 	if chase == true:
 		if anim.animation != "Death":
 			if is_on_floor():
 				velocity.y = JUMP_VELOCITY
 				anim.play("Jump")
+
 			if velocity.y > 0:
 				anim.play("Fall")
+				
 		player = $"../../Player/Foxxi"
 		var direction = (player.position - self.position).normalized()
 		if direction.x > 0:
 			anim.flip_h = true
 		else:
 			anim.flip_h = false
-		velocity.x = direction.x	 * SPEED
+			
+		velocity.x = direction.x * SPEED
 	else:
 		if anim.animation != "Death":
 			anim.play("Idle")
+			
 		velocity.x = 0
 	move_and_slide()
 
@@ -42,6 +48,7 @@ func _on_player_detection_body_exited(body: Node2D) -> void:
 		
 func _on_player_death_body_entered(body: Node2D) -> void:
 	if body.name == "Foxxi":
+		Game.Gold += 5
 		death()
 
 func _on_player_collision_body_entered(body: Node2D) -> void:
@@ -50,9 +57,10 @@ func _on_player_collision_body_entered(body: Node2D) -> void:
 		death()
 	
 func death():
-	Game.Gold += 5
 	Utils.saveGame()
 	chase = false
 	anim.play("Death")
+	$PlayerDeath/Kill.set_deferred("disabled", true)
+	$PlayerCollision/Damage.set_deferred("disabled", true)
 	await anim.animation_finished
 	self.queue_free()
